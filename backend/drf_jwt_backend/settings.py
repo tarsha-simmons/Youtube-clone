eefe27b59b9657c207a1ddc81db8506ca0daae9e
Waltonr <waltonr18126@gmail.com>
2022-05-11 15:32:20 -0400

    Created Reply Model and Comment Model for Youtube App + Registered Youtube App in Project Settings

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
+    'youtube',
     'cars.apps.CarsConfig'  # This app is for example use only
 ]
 
